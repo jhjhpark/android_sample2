79090631f399256bb52e7f6e8b1d1a07500bcfc4
jh park <navith@naver.com>
2015-01-25 17:05:35 +0900

    menu

--- project2/app/src/main/java/com/example/c/t02_criminalintent/CrimeListFragment.java
+++ project2/app/src/main/java/com/example/c/t02_criminalintent/CrimeListFragment.java
@@ -3,6 +3,9 @@ package com.example.c.t02_criminalintent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -26,6 +29,7 @@ public class CrimeListFragment extends ListFragment{
 
         CrimeAdapter adapter = new CrimeAdapter(mCrimes);
         setListAdapter(adapter);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -71,4 +75,33 @@ public class CrimeListFragment extends ListFragment{
         super.onResume();
         ((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_item_new_crime:
+                Crime crime = new Crime();
+                CrimeLab.get(getActivity()).addCrime(crime);
+                Intent intent = new Intent(getActivity(), CrimePagerActivity.class);
+                intent.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
+
+
+
+
+
+
+
+
