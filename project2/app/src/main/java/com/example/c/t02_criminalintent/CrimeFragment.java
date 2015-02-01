ecf27a31ff0cc44b7efd1458b728b5d1a777747d
jh park <navith@naver.com>
2015-02-01 13:14:36 +0900

    camera preview

--- project2/app/src/main/java/com/example/c/t02_criminalintent/CrimeFragment.java
+++ project2/app/src/main/java/com/example/c/t02_criminalintent/CrimeFragment.java
@@ -18,6 +18,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.Date;
 import java.util.UUID;
@@ -36,6 +37,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private ImageButton mPhotoButton;
 
     public CrimeFragment() {
         // Required empty public constructor
@@ -112,6 +114,17 @@ public class CrimeFragment extends Fragment {
 
             }
         });
+
+        mPhotoButton = (ImageButton)v.findViewById(R.id.crime_imageButton);
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CrimeCameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
         return v;
     }
